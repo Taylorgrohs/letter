9da34525023917f6178ef8b9a3d833a077bccd77
Jay Whang and Taylor Grohs <jaywhang83@gmail.com>
2016-01-12 10:08:02 -0800

    Add uppercase to name and letter

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
   $("form").submit(function(event){
-    var name = $("input#name").val();
+    var name = $("input#name").val().toUpperCase();
     $(".name").text(name);
     $("#letter").show();
+    $("#letter").css("text-transform", "uppercase");
     event.preventDefault();
   });
 });
